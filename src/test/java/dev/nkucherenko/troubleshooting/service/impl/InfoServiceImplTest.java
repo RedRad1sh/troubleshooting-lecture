0a38720654a95cbe4fba0f505ba7e5f0fb312755
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2026-02-16 04:30:09 +0300

    add base tests

---
+++ src/test/java/dev/nkucherenko/troubleshooting/service/impl/InfoServiceImplTest.java
@@ -0,0 +1,17 @@
+package dev.nkucherenko.troubleshooting.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author nkucherenko
+ */
+class InfoServiceImplTest {
+
+    @Testb
+    void getInfo() {
+    }
+
+    @Test
+    void editInfo() {
+    }
+}
